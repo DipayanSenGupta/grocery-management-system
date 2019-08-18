93ce5279c559f5b2e0af75dc5842090df1b47d1b
Dipayan Sen Gupta <dip.himu@gmail.com>
2019-08-18 19:39:34 +0600

    Add one to many relationship

--- app/Category.php
+++ app/Category.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
-    //
+    protected $fillable = [
+        'name','aisle'
+    ];
+    public function items(){
+        return $this->hasMany('App\Item');
+    }
+
 }
